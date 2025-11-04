23d70aa920928d66063a7cddd59fc8cbf591423e
nhantam67 <van0939897391@gmail.com>
2025-11-04 08:57:08 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./styles.css";
+//@ts-ignore
+import Home from "./Home";
+//@ts-ignore
+import Layout from "./Layout";
+//@ts-ignore
+import Trang1 from "./Trang1";
+// @ts-ignore
+import Trang2 from "./Trang2";
+// @ts-ignore
+import ListProduct from "./ListProduct.js";
+
+// @ts-ignore
+import ListProducts_SP from "./ListProducts_SP.js";
+
+//@ts-ignore
+import Chitietsanpham from "./Chitietsanpham";
+
+//@ts-ignore
+import ProductDetail from "./ProductDetail";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  // return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ListProducts_SP />} />
+          <Route path="trang1" element={<Trang1 />} />
+          <Route path="trang2" element={<Trang2 />} />
+          <Route path="sanpham/:id" element={<Chitietsanpham />} />
+          <Route path="detail/:id" element={<ProductDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
