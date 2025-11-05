e6a6164f8b0bf62fd3e5f641d8bf55b3e1b9708e
nhantam67 <van0939897391@gmail.com>
2025-11-05 15:35:24 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ocateixuzulwmrtxseom.supabase.co";
+const SUPABASE_URL = "https://xowaxqdflqphajiazubp.supabase.co";
 const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXRlaXh1enVsd21ydHhzZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTYyMTQsImV4cCI6MjA3MTc5MjIxNH0.w7PLqjLGj4hNNGDh81NwDodEUCCqVSVm_PL0FpYWif8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvd2F4cWRmbHFwaGFqaWF6dWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMTEyNjAsImV4cCI6MjA3Nzc4NzI2MH0.wTGFWSVjeGZyCIMIY2ykGpLSkLNYHKeWtz3AX1uJkrU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
