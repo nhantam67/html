eaf40fbf3a5235088e88bbbd2420ab4b0f24f9e1
nhantam67 <van0939897391@gmail.com>
2025-11-05 15:29:26 +0700

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,39 +1,37 @@
-const Footer=()=>
-    {
-     
-        return(
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div class="footer">
+        <div class="container_main">
+          <div class="logo_footer">
+            <a href="">
+              <img
+                src="https://i.pinimg.com/1200x/63/6a/90/636a9061c54d23979a13bf01250a4226.jpg"
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div class="content_footer">
+            <h2>CHIIKAWA NUMBER 1</h2>
+            <p>UNA UNA </p>
 
-            <footer id="footer">
-  <div class="footer">
-    <div class="container_main">
-      <div class="logo_footer">
-        <a href="">
-          <img src="https://giadinh.edu.vn/upload/photo/logofooter-8814.png" alt="Logo"/>
-        </a>
+            <p>Hotline: 0906377373</p>
+
+            <p>Điện thoại: 09398979798</p>
+
+            <p>Email: Chiikawa@gmail.com</p>
+          </div>
+          <div class="banner_footer"></div>
+        </div>
       </div>
-      <div class="content_footer">
-        <h2>TRƯỜNG ĐẠI HỌC GIA ĐỊNH</h2>
-        <p>Cơ sở Tân Sơn Nhất: 371 Nguyễn Kiệm, P.3, Q.Gò Vấp, TP.HCM</p>
-
-<p>Hotline: 0961 12 10 18 - 0962 12 10 18 - 0862 12 10 18</p>
-
-<p>Điện thoại: (028)7301.3456</p>
-
-<p>Email: tuyensinh@giadinh.edu.vn</p>
-      </div> 
-      <div class="banner_footer">
-       
+      <div class="copyright">
+        <div class="container_main">
+          <p>
+            © 2024 Demo bởi Nhân Tâm.<a> | Sitemap</a>
+          </p>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="copyright">
-    <div class="container_main">
-      <p>© 2024 Demo bởi thầy Gia.<a> | Sitemap</a></p>
-    </div>
-  </div>
-</footer>
-
-        );
-
-    }
+    </footer>
+  );
+};
 export default Footer;
