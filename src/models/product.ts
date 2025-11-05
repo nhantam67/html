490e9c8b61a6d7219a83e484704363bcc0db40ae
nhantam67 <van0939897391@gmail.com>
2025-11-05 15:40:35 +0700

    Update product.ts

--- src/models/product.ts
+++ src/models/product.ts
@@ -79,10 +79,10 @@ export const products: Product[] = [
     id: 6,
     title: "Gấu bông Hachiware bánh mì ",
     price: 168,
-    description:
-      "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-    category: "jewelery",
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
+    description: " Gấu bông dễ thương",
+    category: "Gấu bông",
+    image:
+      "https://i.pinimg.com/1200x/56/8e/d0/568ed0d9980a1f820dcde8e927496de7.jpg",
     rating: {
       rate: 3.9,
       count: 70,
@@ -90,12 +90,12 @@ export const products: Product[] = [
   },
   {
     id: 7,
-    title: "White Gold Plated Princess",
-    price: 9.99,
-    description:
-      "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-    category: "jewelery",
-    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
+    title: "Gấu bông hachiware bắc cực",
+    price: 89,
+    description: " Gấu bông dễ thương",
+    category: "Gấu bông",
+    image:
+      "https://i.pinimg.com/1200x/1d/66/47/1d664748dd27f83910fc9bc71b023a21.jpg",
     rating: {
       rate: 3,
       count: 400,
@@ -103,12 +103,12 @@ export const products: Product[] = [
   },
   {
     id: 8,
-    title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    title: "Gấu bông Hachiware bakery",
     price: 10.99,
-    description:
-      "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
-    category: "jewelery",
-    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
+    description: "gấu bông dễ thương",
+    category: "Gấu bông",
+    image:
+      "https://i.pinimg.com/736x/56/6d/b3/566db31363222a4e5bf0b5010da42575.jpg",
     rating: {
       rate: 1.9,
       count: 100,
@@ -116,12 +116,12 @@ export const products: Product[] = [
   },
   {
     id: 9,
-    title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+    title: "Gấu bông Hachiware usagi ",
     price: 64,
-    description:
-      "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-    category: "electronics",
-    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_t.png",
+    description: "gấu bông dễ thương",
+    category: "Gấu bông",
+    image:
+      "https://i.pinimg.com/1200x/91/e5/d6/91e5d6cc460f5877523fe18af71d18c6.jpg",
     rating: {
       rate: 3.3,
       count: 203,
@@ -129,12 +129,12 @@ export const products: Product[] = [
   },
   {
     id: 10,
-    title: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+    title: "Gấu bông Hachiware ",
     price: 109,
-    description:
-      "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
-    category: "electronics",
-    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_t.png",
+    description: " gấu bông dễ thương",
+    category: "gấu bông ",
+    image:
+      "https://i.pinimg.com/1200x/fa/4d/d8/fa4dd88c32ae1fabb33ba9dcffd4e4fc.jpg",
     rating: {
       rate: 2.9,
       count: 470,
@@ -142,13 +142,12 @@ export const products: Product[] = [
   },
   {
     id: 11,
-    title:
-      "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+    title: "Gấu bông Hachiware usagi",
     price: 109,
-    description:
-      "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
-    category: "electronics",
-    image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_t.png",
+    description: ".  gấu bông dễ thương",
+    category: "gấu bông",
+    image:
+      "https://i.pinimg.com/1200x/75/a5/d4/75a5d47cf45665df29f6d8ee121511b0.jpg",
     rating: {
       rate: 4.8,
       count: 319,
@@ -156,13 +155,12 @@ export const products: Product[] = [
   },
   {
     id: 12,
-    title:
-      "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    title: "Gấu bông Hachiware usagi",
     price: 114,
-    description:
-      "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
-    category: "electronics",
-    image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+    description: "gấu bông dễ thương",
+    category: "gấu bông",
+    image:
+      "https://i.pinimg.com/736x/63/d4/c6/63d4c6a9cfe669a7176524b80ac4bcdc.jpg",
     rating: {
       rate: 4.8,
       count: 400,
